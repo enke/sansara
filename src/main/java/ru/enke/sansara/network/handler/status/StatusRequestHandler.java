a8157605e40ffb3c2f1b17d2c817129c4134c3bd
enke <565565+enke@users.noreply.github.com>
2018-02-04 16:18:09 +0300

    Add server status response

---
+++ src/main/java/ru/enke/sansara/network/handler/status/StatusRequestHandler.java
@@ -0,0 +1,34 @@
+package ru.enke.sansara.network.handler.status;
+
+import ru.enke.minecraft.protocol.Protocol;
+import ru.enke.minecraft.protocol.packet.client.status.StatusRequest;
+import ru.enke.minecraft.protocol.packet.data.message.Message;
+import ru.enke.minecraft.protocol.packet.data.message.MessageColor;
+import ru.enke.minecraft.protocol.packet.data.status.Players;
+import ru.enke.minecraft.protocol.packet.data.status.ServerStatusInfo;
+import ru.enke.minecraft.protocol.packet.data.status.Version;
+import ru.enke.minecraft.protocol.packet.server.status.StatusResponse;
+import ru.enke.sansara.Server;
+import ru.enke.sansara.network.handler.MessageHandler;
+import ru.enke.sansara.network.session.NetworkSession;
+
+public class StatusRequestHandler implements MessageHandler<StatusRequest> {
+
+    private final Server server;
+
+    public StatusRequestHandler(final Server server) {
+        this.server = server;
+    }
+
+    @Override
+    public void handle(final NetworkSession session, final StatusRequest statusRequest) {
+        final Version version = new Version(Server.GAME_VERSION, Protocol.VERSION);
+        final Players players = new Players(0, 100);
+        final Message description = new Message("Sansara server", MessageColor.YELLOW);
+        final String favicon = server.getFavicon();
+
+        final ServerStatusInfo statusInfo = new ServerStatusInfo(version, description, players, favicon);
+        session.sendPacket(new StatusResponse(statusInfo));
+    }
+
+}
